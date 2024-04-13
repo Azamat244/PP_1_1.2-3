b57861172227553ede8c781c5c918ec055f7418c
Azamat244 <152506960+Azamat244@users.noreply.github.com>
2024-04-13 12:24:13 +0500

    добавил лог в класс Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,8 +3,10 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Util {
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
     // реализуйте настройку соеденения с БД
     public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DB_URL = "jdbc:mysql://localhost:3306/root";
@@ -16,9 +18,9 @@ public class Util {
         try {
             Class.forName(DB_DRIVER);
             connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
-            System.out.println("Соединение с БД установлено");
+            logger.info("Соединение с БД установлено");
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Соединение с БД не установлено");
+            logger.info("Соединение с БД не установлено");
         }
         return connection;
     }
